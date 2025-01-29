270fb846ff7fbe0e81363af1322beccc057b4836
Joel Smith <joelsmith@tenstorrent.com>
2025-01-29 20:34:09 +0000

    update ioctl.h

--- device/ioctl.h
+++ device/ioctl.h
@@ -1,11 +1,5 @@
-/*
- * SPDX-FileCopyrightText: (c) 2023 Tenstorrent Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// clang-format off
-// This file is copied from KMD, so we don't want clang formatting diff.
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+// SPDX-License-Identifier: GPL-2.0-only
 
 #ifndef TTDRIVER_IOCTL_H_INCLUDED
 #define TTDRIVER_IOCTL_H_INCLUDED
@@ -25,6 +19,9 @@
 #define TENSTORRENT_IOCTL_GET_DRIVER_INFO	_IO(TENSTORRENT_IOCTL_MAGIC, 5)
 #define TENSTORRENT_IOCTL_RESET_DEVICE		_IO(TENSTORRENT_IOCTL_MAGIC, 6)
 #define TENSTORRENT_IOCTL_PIN_PAGES		_IO(TENSTORRENT_IOCTL_MAGIC, 7)
+#define TENSTORRENT_IOCTL_LOCK_CTL		_IO(TENSTORRENT_IOCTL_MAGIC, 8)
+#define TENSTORRENT_IOCTL_MAP_PEER_BAR		_IO(TENSTORRENT_IOCTL_MAGIC, 9)
+#define TENSTORRENT_IOCTL_UNPIN_PAGES		_IO(TENSTORRENT_IOCTL_MAGIC, 10)
 
 // For tenstorrent_mapping.mapping_id. These are not array indices.
 #define TENSTORRENT_MAPPING_UNUSED		0
@@ -35,7 +32,9 @@
 #define TENSTORRENT_MAPPING_RESOURCE2_UC	5
 #define TENSTORRENT_MAPPING_RESOURCE2_WC	6
 
-#define TENSTORRENT_MAX_DMA_BUFS	8
+#define TENSTORRENT_MAX_DMA_BUFS	256
+
+#define TENSTORRENT_RESOURCE_LOCK_COUNT 64
 
 struct tenstorrent_get_device_info_in {
 	__u32 output_size_bytes;
@@ -48,8 +47,8 @@ struct tenstorrent_get_device_info_out {
 	__u16 subsystem_vendor_id;
 	__u16 subsystem_id;
 	__u16 bus_dev_fn;	// [0:2] function, [3:7] device, [8:15] bus
-	__u16 max_dma_buf_size_log2;
-	__u16 pci_domain;
+	__u16 max_dma_buf_size_log2;	// Since 1.0
+	__u16 pci_domain;		// Since 1.23
 };
 
 struct tenstorrent_get_device_info {
@@ -123,6 +122,11 @@ struct tenstorrent_get_driver_info {
 	struct tenstorrent_get_driver_info_out out;
 };
 
+// tenstorrent_reset_device_in.flags
+#define TENSTORRENT_RESET_DEVICE_RESTORE_STATE 0
+#define TENSTORRENT_RESET_DEVICE_RESET_PCIE_LINK 1
+#define TENSTORRENT_RESET_DEVICE_CONFIG_WRITE 2
+
 struct tenstorrent_reset_device_in {
 	__u32 output_size_bytes;
 	__u32 flags;
@@ -139,7 +143,9 @@ struct tenstorrent_reset_device {
 };
 
 // tenstorrent_pin_pages_in.flags
-#define TENSTORRENT_PIN_PAGES_CONTIGUOUS 1
+#define TENSTORRENT_PIN_PAGES_CONTIGUOUS 1	// app attests that the pages are physically contiguous
+#define TENSTORRENT_PIN_PAGES_WC 2
+#define TENSTORRENT_PIN_PAGES_UC 4
 
 struct tenstorrent_pin_pages_in {
 	__u32 output_size_bytes;
@@ -152,10 +158,62 @@ struct tenstorrent_pin_pages_out {
 	__u64 physical_address;
 };
 
+// unpinning subset of a pinned buffer is not supported
+struct tenstorrent_unpin_pages_in {
+	__u64 virtual_address;	// original VA used to pin, not current VA if remapped
+	__u64 size;
+	__u64 reserved;
+};
+
+struct tenstorrent_unpin_pages_out {
+};
+
+struct tenstorrent_unpin_pages {
+	struct tenstorrent_unpin_pages_in in;
+	struct tenstorrent_unpin_pages_out out;
+};
+
 struct tenstorrent_pin_pages {
 	struct tenstorrent_pin_pages_in in;
 	struct tenstorrent_pin_pages_out out;
 };
 
+// tenstorrent_lock_ctl_in.flags
+#define TENSTORRENT_LOCK_CTL_ACQUIRE 0
+#define TENSTORRENT_LOCK_CTL_RELEASE 1
+#define TENSTORRENT_LOCK_CTL_TEST 2
+
+struct tenstorrent_lock_ctl_in {
+	__u32 output_size_bytes;
+	__u32 flags;
+	__u8  index;
+};
+
+struct tenstorrent_lock_ctl_out {
+	__u8 value;
+};
+
+struct tenstorrent_lock_ctl {
+	struct tenstorrent_lock_ctl_in in;
+	struct tenstorrent_lock_ctl_out out;
+};
+
+struct tenstorrent_map_peer_bar_in {
+	__u32 peer_fd;
+	__u32 peer_bar_index;
+	__u32 peer_bar_offset;
+	__u32 peer_bar_length;
+	__u32 flags;
+};
+
+struct tenstorrent_map_peer_bar_out {
+	__u64 dma_address;
+	__u64 reserved;
+};
+
+struct tenstorrent_map_peer_bar {
+	struct tenstorrent_map_peer_bar_in in;
+	struct tenstorrent_map_peer_bar_out out;
+};
+
 #endif
-// clang-format on
